2de8a70f819d5fdbdc26e32426b0795f63f63605
Pedro Godoi <pedrogodoiriva@gmail.com>
2025-02-05 22:54:44 -0300

    Initializing classic page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import Link from "next/link";
 
-
 export default function Home() {
   return (
     <div className="flex flex-col justify-between items-center p-10 h-screen">
-
-      <div className="flex flex-row bg-linear-to-b from-zinc-700 to-zinc-700">
+      <div className="flex flex-row ">
         <h1 className="font-semibold text-7xl bg-clip-text bg-gradient-to-b from-red-900 to-red-600 text-transparent">Cine</h1>
         <h1 className="font-semibold text-7xl bg-clip-text bg-gradient-to-b from-zinc-900 to-zinc-50 text-transparent">dle</h1>
       </div>
