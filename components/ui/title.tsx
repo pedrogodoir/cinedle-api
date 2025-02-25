e68fecb20f4f66dbad915bb14216714bb52c8b36
Pedro Godoi <pedrogodoiriva@gmail.com>
2025-02-25 15:33:37 -0300

    adjusting title colors

--- components/ui/title.tsx
+++ components/ui/title.tsx
@@ -1,11 +1,11 @@
 import * as React from "react"
 
 interface TitleProps {
-  variant?: "default" | "red" | "blue",
+  variant?: "default" | "red" | "blue" | "white",
   size?: "default" | "lg" | "xl",
   href?: string,
   children: React.ReactNode,
-  className: React.ReactNode
+  className?: React.ReactNode
 }
 
 const Title = React.forwardRef<HTMLDivElement, TitleProps>(
@@ -14,7 +14,8 @@ const Title = React.forwardRef<HTMLDivElement, TitleProps>(
     const variants = {
       default: "",
       red: " bg-clip-text bg-gradient-to-b from-red-900 to-red-600 text-transparent",
-      blue: " bg-clip-text bg-gradient-to-b from-sky-900 to-sky-600 text-transparent"
+      blue: " bg-clip-text bg-gradient-to-b from-sky-900 to-sky-600 text-transparent",
+      white: " bg-clip-text bg-gradient-to-b from-zinc-900 to-zinc-50 text-transparent"
     }
 
     const sizes = {
